622d7975cf7b4a7d22429a3564fb7cd8510e82a0
Trh <38323745+aka40x@users.noreply.github.com>
2026-03-06 15:58:54 +0700

    add validate

--- MiniTest/script.js
+++ MiniTest/script.js
@@ -3,6 +3,8 @@ const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const rePasswordInput = document.getElementById("rePassword");
 
+let userList = []
+
 form.addEventListener("submit", function(event){
 
     event.preventDefault(); 
@@ -11,12 +13,23 @@ form.addEventListener("submit", function(event){
     const password = passwordInput.value;
     const rePassword = rePasswordInput.value;
 
+    let checkEmail = userList.find(user => user.email === email)
+    if(checkEmail){
+        alert("email đã tồn tại r cưng")
+        return
+    }
+
+    if(password !== rePassword){
+        alert("Mật khẩu phải giống nhau")
+        return
+    }
+
     const user = {
         email: email,
         password: password,
         rePassword: rePassword
     };
 
-    
+    userList.push(user)
     console.log(user);
 });
